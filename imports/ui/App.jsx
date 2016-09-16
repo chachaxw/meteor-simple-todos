29a55f75012218606fc3124fd33e7f104bb1c7d5
chachaxw <867571123@qq.com>
2016-09-16 14:22:15 +0800

    add login button

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import Task from './Task.jsx';
+import AccountsUIWrapper from './accountsUIWrapper.jsx';
 import { Tasks } from '../api/tasks.js';
 
 // App component - represents the whole app
@@ -12,9 +13,19 @@ class App extends Component {
   //     ...this.props.tasks, //tasks from MongoDB
   //   ];
   // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideCompleted: false,
+    };
+  }
 
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -23,13 +34,17 @@ class App extends Component {
     e.preventDefault();
 
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
     Tasks.insert({
       text,
       createdAt: new Date(),
     });
-
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
   }
 
   render() {
@@ -37,7 +52,17 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+          <AccountsUIWrapper />
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
@@ -56,10 +81,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(), // tasks will be sorted by createdAt
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
